2c35fb62abcd2783d8fa35ed1f6ff4fa830cdf3f
GabrieleRizzo19 <gabryrizz02@gmail.com>
2023-08-25 19:35:01 +0200

    deleted surname and add photo in config doctors

--- config/doctors.php
+++ config/doctors.php
@@ -2,191 +2,170 @@
 
 return [
         [
-            'name'=> 'Andrea',
-            'surname' => 'Grande',
+            'name'=> 'Andrea Grande',
             'address'=>'Via del Magnanimo 7',
             'city'=>'Roma',
-            'image'=>'https://www.numerochiuso.info/wp-content/uploads/2022/11/medico-di-famiglia.jpg',
+            'image'=>'uploads/medico.jpg',
             'curriculum'=>'cvmedico.pdf',
             'phone_number'=>'3891681144'
         ],
         [
             'name'=> 'Bartolomeo',
-            'surname' => 'Trevisan',
             'address'=>'Via Moro 81',
             'city'=>'Treviso',
-            'image'=>'https://www.numerochiuso.info/wp-content/uploads/2022/11/medico-di-famiglia.jpg',
+            'image'=>'uploads/medico.jpg',
             'curriculum'=>'cvmedico.pdf',
             'phone_number'=>'3331118784'
         ],
         [
             'name'=> 'Carlo',
-            'surname' => 'Demografico',
             'address'=>'Via Giotto 91',
             'city'=>'Milano',
-            'image'=>'https://www.numerochiuso.info/wp-content/uploads/2022/11/medico-di-famiglia.jpg',
+            'image'=>'uploads/medico.jpg',
             'curriculum'=>'cvmedico.pdf',
             'phone_number'=>'3471094124',
         ],
         [
             'name'=> 'Damiano',
-            'surname' => 'Donatelli',
             'address'=>'Via Liguria 12',
             'city'=>'Torino',
-            'image'=>'https://www.numerochiuso.info/wp-content/uploads/2022/11/medico-di-famiglia.jpg',
+            'image'=>'uploads/medico.jpg',
             'curriculum'=>'cvmedico.pdf',
             'phone_number'=>'355127198'
         ],
         [
             'name'=> 'Eraldo',
-            'surname' => 'Prosciutto',
             'address'=>'Via Sardegna 99',
             'city'=>'Napoli',
-            'image'=>'https://www.numerochiuso.info/wp-content/uploads/2022/11/medico-di-famiglia.jpg',
+            'image'=>'uploads/medico.jpg',
             'curriculum'=>'cvmedico.pdf',
             'phone_number'=>'3201019222'
         ],
         [
             'name'=> 'Floriana',
-            'surname' => 'Ciccipaglia',
             'address'=>'Via del Santuario 9',
             'city'=>'Campobasso',
-            'image'=>'https://www.numerochiuso.info/wp-content/uploads/2022/11/medico-di-famiglia.jpg',
+            'image'=>'uploads/medico.jpg',
             'curriculum'=>'cvmedico.pdf',
             'phone_number'=>'3819012455'
         ],
         [
             'name'=> 'Gabriele',
-            'surname' => 'Coppola',
             'address'=>'Piazza Alcide De Gasperi 44',
             'city'=>'Padova',
-            'image'=>'https://www.numerochiuso.info/wp-content/uploads/2022/11/medico-di-famiglia.jpg',
+            'image'=>'uploads/medico.jpg',
             'curriculum'=>'cvmedico.pdf',
             'phone_number'=>'3115648123'
         ],
         [
             'name'=> 'Hermione',
-            'surname' => 'Gregoriani',
             'address'=>'Via del Salmone',
             'city'=>'Bari',
-            'image'=>'https://www.numerochiuso.info/wp-content/uploads/2022/11/medico-di-famiglia.jpg',
+            'image'=>'uploads/medico.jpg',
             'curriculum'=>'cvmedico.pdf',
             'phone_number'=>'3319089733'
         ],
         [
             'name'=> 'Ilaria',
-            'surname' => 'Fallacci',
             'address'=>'Via del Mulino 81',
             'city'=>'Mantova',
-            'image'=>'https://www.numerochiuso.info/wp-content/uploads/2022/11/medico-di-famiglia.jpg',
+            'image'=>'uploads/medico.jpg',
             'curriculum'=>'cvmedico.pdf',
             'phone_number'=>'3489088901'
         ],
         [
             'name'=> 'Luaro',
-            'surname' => 'Pauro',
             'address'=>'Via del Sauro 12',
             'city'=>'Sbrunchio sul mento',
-            'image'=>'https://www.numerochiuso.info/wp-content/uploads/2022/11/medico-di-famiglia.jpg',
+            'image'=>'uploads/medico.jpg',
             'curriculum'=>'cvmedico.pdf',
             'phone_number'=>'3411890123'
         ],
         [
-            'name'=> 'Marco',
-            'surname' => 'Rossi',
+            'name'=> 'Matteo',
             'address'=>'Via Consueta 1',
             'city'=>'Paese',
-            'image'=>'https://www.numerochiuso.info/wp-content/uploads/2022/11/medico-di-famiglia.jpg',
+            'image'=>'uploads/medico.jpg',
             'curriculum'=>'cvmedico.pdf',
             'phone_number'=>'3333333333'
         ],
         [
             'name'=> 'Norman',
-            'surname' => 'Bates',
             'address'=>'Via dell Hotel 18',
             'city'=>'Firenze',
-            'image'=>'https://www.numerochiuso.info/wp-content/uploads/2022/11/medico-di-famiglia.jpg',
+            'image'=>'uploads/medico.jpg',
             'curriculum'=>'cvmedico.pdf',
             'phone_number'=>'3901990133'
         ],
         [
             'name'=> 'Orlando',
-            'surname' => 'Canzoni',
             'address'=>'Via popolare',
             'city'=>'Urbino',
-            'image'=>'https://www.numerochiuso.info/wp-content/uploads/2022/11/medico-di-famiglia.jpg',
+            'image'=>'uploads/medico.jpg',
             'curriculum'=>'cvmedico.pdf',
             'phone_number'=>'3489019202'
         ],
         [
             'name'=> 'Paolo',
-            'surname' => 'Fiordalisi',
             'address'=>'Via del Fangario 23',
             'city'=>'Cagliari',
-            'image'=>'https://www.numerochiuso.info/wp-content/uploads/2022/11/medico-di-famiglia.jpg',
+            'image'=>'uploads/medico.jpg',
             'curriculum'=>'cvmedico.pdf',
             'phone_number'=>'3891790189'
         ],
         [
             'name'=> 'Quasimodo',
-            'surname' => 'Salvatore',
             'address'=>'Via del Poeta 45',
             'city'=>'Genova',
-            'image'=>'https://www.numerochiuso.info/wp-content/uploads/2022/11/medico-di-famiglia.jpg',
+            'image'=>'uploads/medico.jpg',
             'curriculum'=>'cvmedico.pdf',
             'phone_number'=>'37898091199'
         ],
         [
             'name'=> 'Rodolfo',
-            'surname' => 'Padinolfo',
             'address'=>'Via Ernesto Forza 17',
             'city'=>'Palermo',
-            'image'=>'https://www.numerochiuso.info/wp-content/uploads/2022/11/medico-di-famiglia.jpg',
+            'image'=>'uploads/medico.jpg',
             'curriculum'=>'cvmedico.pdf',
             'phone_number'=>'3567809123'
         ],
         [
             'name'=> 'Sara',
-            'surname' => 'Fanna',
             'address'=>'Via Giacomo Matteotti 26',
             'city'=>'Potenza',
-            'image'=>'https://www.numerochiuso.info/wp-content/uploads/2022/11/medico-di-famiglia.jpg',
+            'image'=>'uploads/medico.jpg',
             'curriculum'=>'cvmedico.pdf',
             'phone_number'=>'3908918901'
         ],
         [
             'name'=> 'Tiziano',
-            'surname' => 'Aspetti',
             'address'=>'Via dell Artista',
             'city'=>'Siena',
-            'image'=>'https://www.numerochiuso.info/wp-content/uploads/2022/11/medico-di-famiglia.jpg',
+            'image'=>'uploads/medico.jpg',
             'curriculum'=>'cvmedico.pdf',
             'phone_number'=>'3789018909'
         ],
         [
             'name'=> 'Ubaldo',
-            'surname' => 'Usai',
             'address'=>'Via delle Vele',
             'city'=>'Perugia',
-            'image'=>'https://www.numerochiuso.info/wp-content/uploads/2022/11/medico-di-famiglia.jpg',
+            'image'=>'uploads/medico.jpg',
             'curriculum'=>'cvmedico.pdf',
             'phone_number'=>'3901890788'
         ],
         [
             'name'=> 'Valeria',
-            'surname' => 'Carrà',
             'address'=>'Via della Montagna 12',
             'city'=>'Udine',
-            'image'=>'https://www.numerochiuso.info/wp-content/uploads/2022/11/medico-di-famiglia.jpg',
+            'image'=>'uploads/medico.jpg',
             'curriculum'=>'cvmedico.pdf',
             'phone_number'=>'3389018902'
         ],
         [
             'name'=> 'Zaira',
-            'surname' => 'Frassica',
             'address'=>'Via Marche 14',
             'city'=>'Bolzano',
-            'image'=>'https://www.numerochiuso.info/wp-content/uploads/2022/11/medico-di-famiglia.jpg',
+            'image'=>'uploads/medico.jpg',
             'curriculum'=>'cvmedico.pdf',
             'phone_number'=>'3891790188'
         ]
