cc8315ebad14a41241d4bd0c0f70d8255167255c
GabrieleRizzo19 <gabryrizz02@gmail.com>
2023-08-26 17:15:17 +0200

    edited dashboard with welcome message

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,23 +1,9 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container-fluid mt-4">
+    <div class="container mt-3">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        {{ __('You are logged in!') }}
-                    </div>
-                </div>
-            </div>
+            <h1> Ciao {{ $user_name }}</h1>
         </div>
     </div>
 @endsection
