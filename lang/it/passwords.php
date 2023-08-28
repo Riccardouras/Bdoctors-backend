3b43d12b72d5c809cafb6be78ed3b40fa09f220a
GabrieleRizzo19 <gabryrizz02@gmail.com>
2023-08-28 19:57:26 +0200

    add it lang folder with translated error messages

---
+++ lang/it/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'La tua password è stata resettata.',
+    'sent' => 'Ti abbiamo inviato per email il link per resettare la tua password.',
+    'throttled' => 'Aspetta prima di riprovare.',
+    'token' => 'Questo token di reset password non è valido.',
+    'user' => "Non esiste nessun utente con questa mail.",
+
+];
