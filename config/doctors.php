eed2a7c5123462a8bf04b9914d71e153997cf0fe
GabrieleRizzo19 <gabryrizz02@gmail.com>
2023-08-30 18:47:01 +0200

    update doctors,specialties and sponsor data

--- config/doctors.php
+++ config/doctors.php
@@ -2,171 +2,213 @@
 
 return [
         [
-            'name'=> 'Andrea',
+            'name'=> 'Andrea Grande',
             'address'=>'Via del Magnanimo 7',
             'city'=>'Roma',
             'image'=>'uploads/medico.jpg',
             'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3891681144'
+            'phone_number'=>'3891681144',
+            'specialties' => ['1'],
+            'service' => 'Diagnosi e cura delle principali manifestazioni allergiche'
         ],
         [
-            'name'=> 'Bartolomeo',
+            'name'=> 'Bartolomeo Bartino',
             'address'=>'Via Moro 81',
             'city'=>'Treviso',
             'image'=>'uploads/medico2.jpg',
             'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3331118784'
+            'phone_number'=>'3331118784',
+            'specialties' => ['2', '1'],
+            'service' => 'Esami istologici. Diagnosi e cura delle principali manifestazioni allergiche'
         ],
         [
-            'name'=> 'Carlo',
+            'name'=> 'Carlo Magno',
             'address'=>'Via Giotto 91',
             'city'=>'Milano',
             'image'=>'uploads/medico3.jpg',
             'curriculum'=>'uploads/cvmedico.pdf',
             'phone_number'=>'3471094124',
+            'specialties' => ['3', '2', '1'],
+            'service' => 'Ripristino funzionalità epato-renale. Esami istologici. Diagnosi e cura delle principali manifestazioni allergiche'
         ],
         [
-            'name'=> 'Damiano',
+            'name'=> 'Damiano Ligure',
             'address'=>'Via Liguria 12',
             'city'=>'Torino',
             'image'=>'uploads/medico4.jpg',
             'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'355127198'
+            'phone_number'=>'355127198',
+            'specialties' => ['4'],
+            'service' => 'Controllo al cuore.'
         ],
         [
-            'name'=> 'Eraldo',
+            'name'=> 'Eraldo Bossi',
             'address'=>'Via Sardegna 99',
             'city'=>'Napoli',
             'image'=>'uploads/medico5.jpg',
             'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3201019222'
+            'phone_number'=>'3201019222',
+            'specialties' => ['5'],
+            'service' => 'Controllo al cuore dei bambini'
         ],
         [
-            'name'=> 'Floriana',
+            'name'=> 'Floriana Berlusconi',
             'address'=>'Via del Santuario 9',
             'city'=>'Campobasso',
             'image'=>'uploads/medico16.jpg',
             'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3819012455'
+            'phone_number'=>'3819012455',
+            'specialties' => ['6'],
+            'service' => 'Intervento chirurgico cavità addominale'
         ],
         [
-            'name'=> 'Gabriele',
+            'name'=> 'Gabriele Rizzo',
             'address'=>'Piazza Alcide De Gasperi 44',
             'city'=>'Padova',
             'image'=>'uploads/medico6.jpg',
             'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3115648123'
+            'phone_number'=>'3115648123',
+            'specialties' => ['7'],
+            'service' => 'Intervento chirurgico cavità addominale dei bambini'
         ],
         [
-            'name'=> 'Hermione',
+            'name'=> 'Hermione Granger',
             'address'=>'Via del Salmone',
             'city'=>'Bari',
             'image'=>'uploads/medico17.jpg',
             'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3319089733'
+            'phone_number'=>'3319089733',
+            'specialties' => ['8'],
+            'service' => 'Checkup dermatologico e dermatoscopie.'
         ],
         [
-            'name'=> 'Ilaria',
+            'name'=> 'Ilaria Condizionata',
             'address'=>'Via del Mulino 81',
             'city'=>'Mantova',
             'image'=>'uploads/medico18.jpg',
             'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3489088901'
+            'phone_number'=>'3489088901',
+            'specialties' => ['9'],
+            'service' => 'Esami del sangue'
         ],
         [
-            'name'=> 'Laura',
+            'name'=> 'Laura Pausini',
             'address'=>'Via del Sauro 12',
             'city'=>'Sbrunchio sul mento',
             'image'=>'uploads/medico19.jpg',
             'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3411890123'
+            'phone_number'=>'3411890123',
+            'specialties' => ['10'],
+            'service' => 'Visita di controllo'
         ],
         [
-            'name'=> 'Matteo',
+            'name'=> 'Matteo Galoni',
             'address'=>'Via Consueta 1',
             'city'=>'Paese',
             'image'=>'uploads/medico7.jpg',
             'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3333333333'
+            'phone_number'=>'3333333333',
+            'specialties' => ['11'],
+            'service' => 'Visita di controllo'
         ],
         [
-            'name'=> 'Norman',
+            'name'=> 'Norman Dia',
             'address'=>'Via dell Hotel 18',
             'city'=>'Firenze',
             'image'=>'uploads/medico8.jpg',
             'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3901990133'
+            'phone_number'=>'3901990133',
+            'specialties' => ['12'],
+            'service' => 'Visita di controllo'
         ],
         [
-            'name'=> 'Orlando',
+            'name'=> 'Orlando Furioso',
             'address'=>'Via popolare',
             'city'=>'Urbino',
             'image'=>'uploads/medico9.jpg',
             'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3489019202'
+            'phone_number'=>'3489019202',
+            'specialties' => ['13'],
+            'service' => 'Visita di controllo'
         ],
         [
-            'name'=> 'Paolo',
+            'name'=> 'Paolo Rossi',
             'address'=>'Via del Fangario 23',
             'city'=>'Cagliari',
             'image'=>'uploads/medico10.jpg',
             'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3891790189'
+            'phone_number'=>'3891790189',
+            'specialties' => ['14'],
+            'service' => 'Visita di controllo'
         ],
         [
-            'name'=> 'Quasimodo',
+            'name'=> 'Quasimodo Meloni',
             'address'=>'Via del Poeta 45',
             'city'=>'Genova',
             'image'=>'uploads/medico11.jpg',
             'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'37898091199'
+            'phone_number'=>'37898091199',
+            'specialties' => ['15'],
+            'service' => 'Visita di controllo'
         ],
         [
-            'name'=> 'Rodolfo',
+            'name'=> 'Rodolfo Valentino',
             'address'=>'Via Ernesto Forza 17',
             'city'=>'Palermo',
             'image'=>'uploads/medico12.jpg',
             'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3567809123'
+            'phone_number'=>'3567809123',
+            'specialties' => ['16'],
+            'service' => 'Visita di controllo'
         ],
         [
-            'name'=> 'Sara',
+            'name'=> 'Sara Mai',
             'address'=>'Via Giacomo Matteotti 26',
             'city'=>'Potenza',
             'image'=>'uploads/medico20.jpg',
             'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3908918901'
+            'phone_number'=>'3908918901',
+            'specialties' => ['17'],
+            'service' => 'Visita di controllo'
         ],
         [
-            'name'=> 'Tiziano',
+            'name'=> 'Tiziano Ferro',
             'address'=>'Via dell Artista',
             'city'=>'Siena',
             'image'=>'uploads/medico13.jpg',
             'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3789018909'
+            'phone_number'=>'3789018909',
+            'specialties' => ['18'],
+            'service' => 'Visita di controllo'
         ],
         [
-            'name'=> 'Ubaldo',
+            'name'=> 'Ubaldo Ubaldi',
             'address'=>'Via delle Vele',
             'city'=>'Perugia',
             'image'=>'uploads/medico14.jpg',
             'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3901890788'
+            'phone_number'=>'3901890788',
+            'specialties' => ['19'],
+            'service' => 'Visita di controllo'
         ],
         [
-            'name'=> 'Valeria',
+            'name'=> 'Valeria Caramellina',
             'address'=>'Via della Montagna 12',
             'city'=>'Udine',
             'image'=>'uploads/medico21.jpg',
             'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3389018902'
+            'phone_number'=>'3389018902',
+            'specialties' => [],
+            'service' => 'Visita di controllo'
         ],
         [
-            'name'=> 'Zaira',
+            'name'=> 'Zaira Orfei',
             'address'=>'Via Marche 14',
             'city'=>'Bolzano',
             'image'=>'uploads/medico22.jpg',
             'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3891790188'
+            'phone_number'=>'3891790188',
+            'specialties' => [],
+            'service' => 'Visita di controllo'
         ]
 ];
