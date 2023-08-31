94c7644d3864192fefe436bed6581a507137f965
MatteoGaloni <mtgaloni@gmail.com>
2023-08-31 12:04:01 +0200

    add messages and reviews status

--- resources/views/admin/doctors/messages.blade.php
+++ resources/views/admin/doctors/messages.blade.php
@@ -3,30 +3,34 @@
 @section('content')
     <div class="container">
         <div class="row">
-                <h1>MESSAGGI RICEVUTI</h1>
+            <h1>MESSAGGI RICEVUTI</h1>
 
-                @foreach ($messages as $message)
-                    <div class="col-12">
-                        <div class="card mb-3 ">
-                            <div class="card-header">
-                                <div class="card-title">
-                                    <h4>Mittente: {{ $message->full_name }}</h4>
-                                </div>
-                                <div class="card-subtitle">
-                                    Mail: {{ $message->mail }} <br>
-                                    {{ $message->date }}
-                                </div>
-                            </div>
-                            <div class="card-body">
-                                <p>
-                                    <h5>Testo del messaggio:</h5>
-                                    {{ $message->text }}
-                                </p>
-                            </div>
+            @if (count($messages) == 0)
+                <h2>Non ci sono messaggi da visualizzare</h2>
+            @endif
 
+            @foreach ($messages as $message)
+                <div class="col-12">
+                    <div class="card mb-3 ">
+                        <div class="card-header">
+                            <div class="card-title">
+                                <h4>Mittente: {{ $message->full_name }}</h4>
+                            </div>
+                            <div class="card-subtitle">
+                                Mail: {{ $message->mail }} <br>
+                                {{ $message->date }}
+                            </div>
                         </div>
+                        <div class="card-body">
+                            <p>
+                            <h5>Testo del messaggio:</h5>
+                            {{ $message->text }}
+                            </p>
+                        </div>
+
                     </div>
-                @endforeach
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
--- resources/views/admin/doctors/reviews.blade.php
+++ resources/views/admin/doctors/reviews.blade.php
@@ -3,30 +3,34 @@
 @section('content')
     <div class="container">
         <div class="row">
-                <h1>RECENSIONI RICEVUTE</h1>
+            <h1>RECENSIONI RICEVUTE</h1>
 
-                @foreach ($reviews as $review)
-                    <div class="col-12">
-                        <div class="card mb-3 ">
-                            <div class="card-header">
-                                <div class="card-title">
-                                    <h4>{{ $review->title }}</h4>
-                                </div>
-                                <div class="card-subtitle">
-                                    {{ $review->name }} <br>
-                                    {{ $review->date }}
-                                </div>
-                            </div>
-                            <div class="card-body">
-                                <p>
-                                    <h5>Testo della recensione:</h5>
-                                    {{ $review->comment }}
-                                </p>
-                            </div>
+            @if (count($reviews) == 0)
+                <h2>Non ci sono recensioni da visualizzare</h2>
+            @endif
 
+            @foreach ($reviews as $review)
+                <div class="col-12">
+                    <div class="card mb-3 ">
+                        <div class="card-header">
+                            <div class="card-title">
+                                <h4>{{ $review->title }}</h4>
+                            </div>
+                            <div class="card-subtitle">
+                                {{ $review->name }} <br>
+                                {{ $review->date }}
+                            </div>
                         </div>
+                        <div class="card-body">
+                            <p>
+                            <h5>Testo della recensione:</h5>
+                            {{ $review->comment }}
+                            </p>
+                        </div>
+
                     </div>
-                @endforeach
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
