7140289fc8679c36172965a6ff6fd5a7ac92a1c5
MatteoGaloni <mtgaloni@gmail.com>
2023-09-06 18:22:41 +0200

    add only active sponsored

--- resources/views/admin/sponsorship/form.blade.php
+++ resources/views/admin/sponsorship/form.blade.php
@@ -25,7 +25,7 @@
                             <div class="card-body">
                                 <h5 class="card-title">Sponsorizza il tuo profilo per {{ $sponsor->hours }} ore </h5>
                                 <p class="card-text">{{ $sponsor->price }} €</p>
-                                <input type="radio" name="selected_package" value="{{ $sponsor->id }}"> Seleziona
+                                <input type="radio" name="selected_package" checked value="{{ $sponsor->id }}"> Seleziona
                             </div>
                         </div>
                     </div>
@@ -33,5 +33,19 @@
             </div>
             <button type="submit" class="btn btn-warning mt-2">Passa ai dettagli del pagamento</button>
         </form>
+
+        <div class="row">
+            @foreach ($sponsoredDoctors as $item)
+                <div class="col-4">
+                    <div class="card mt-2">
+                        <div class="card-body">
+                            <div class="card-title">Sponsorizzazione attiva </div>
+                            <div class="card-text">Data inizio: {{ $item->start_date }}</div>
+                            <div class="card-text">Data fine: {{ $item->end_date }}</div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
