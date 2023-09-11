89a6c6baf7a7bde76ed604393d33fd26acf1aaba
MatteoGaloni <mtgaloni@gmail.com>
2023-09-11 13:04:24 +0200

    fix reviews in dashboard

--- resources/views/admin/doctors/messages.blade.php
+++ resources/views/admin/doctors/messages.blade.php
@@ -10,65 +10,57 @@
             @endif
         </header-section>
     </div>
-    @if (count($messages) == 0)
-        <h2 class="text-center">Non ci sono messaggi da visualizzare</h2>
 
-        <div class="container">
-        @else
-            <!-- Pannello delle email -->
-            <div class="card border-0">
-                <div class="card-body">
-                    <table class="my-table table table-hover table-striped">
-                        <thead>
-                            <tr>
-                                <th class="d-none d-md-table-cell">Da <i class="fa-solid fa-user"></th>
-                                <th>Mail <i class="fa-solid fa-envelope"></i></th>
-                                <th class="d-none d-sm-table-cell">Data <i class="fa-regular fa-calendar"></i></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($messages as $message)
-                                <tr class="messandreview_bg pointer " data-bs-toggle="modal"
-                                    data-bs-target="#message{{ $loop->index }}">
-                                    <td class="d-none d-md-table-cell">{{ $message->full_name }}</td>
-                                    <td>{{ $message->mail }}</td>
-                                    <td class="d-none d-sm-table-cell">{{ $message->date }}</td>
-                                </tr>
+    <!-- Pannello delle email -->
+    <div class="card border-0">
+        <div class="card-body">
+            <table class="my-table table table-hover table-striped">
+                <thead>
+                    <tr>
+                        <th class="d-none d-md-table-cell">Da <i class="fa-solid fa-user"></th>
+                        <th>Mail <i class="fa-solid fa-envelope"></i></th>
+                        <th class="d-none d-sm-table-cell">Data <i class="fa-regular fa-calendar"></i></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($messages as $message)
+                        <tr class="messandreview_bg pointer " data-bs-toggle="modal"
+                            data-bs-target="#message{{ $loop->index }}">
+                            <td class="d-none d-md-table-cell">{{ $message->full_name }}</td>
+                            <td>{{ $message->mail }}</td>
+                            <td class="d-none d-sm-table-cell">{{ $message->date }}</td>
+                        </tr>
 
-                                {{-- modale --}}
-                                <div class="modal fade bg" id="message{{ $loop->index }}" data-bs-backdrop="static"
-                                    data-bs-keyboard="false" tabindex="-1"
-                                    aria-labelledby="messageLabel{{ $loop->index }}" aria-hidden="true">
-                                    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <div>
-                                                    <h1 class="modal-title fs-5" id="messageLabel{{ $loop->index }}">
-                                                        {{ $message->full_name }}
-                                                    </h1>
-                                                    <h5><a href="mailto:{{ $message->mail }}">{{ $message->mail }}</a>
-                                                    </h5>
-                                                </div>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                    aria-label="Close"></button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <h5>Testo del messaggio:</h5>
-                                                <p>{{ $message->text }}</p>
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn_close"
-                                                    data-bs-dismiss="modal">Close</button>
-                                            </div>
+                        {{-- modale --}}
+                        <div class="modal fade bg" id="message{{ $loop->index }}" data-bs-backdrop="static"
+                            data-bs-keyboard="false" tabindex="-1" aria-labelledby="messageLabel{{ $loop->index }}"
+                            aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <div>
+                                            <h3 class="modal-title" id="messageLabel{{ $loop->index }}">
+                                                {{ $message->full_name }}
+                                            </h3>
+                                            <h5><a href="mailto:{{ $message->mail }}">{{ $message->mail }}</a>
+                                            </h5>
                                         </div>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <h5>Testo del messaggio:</h5>
+                                        <p>{{ $message->text }}</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn_close" data-bs-dismiss="modal">Close</button>
                                     </div>
                                 </div>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
-    @endif
     </div>
 @endsection
--- resources/views/admin/doctors/reviews.blade.php
+++ resources/views/admin/doctors/reviews.blade.php
@@ -1,93 +1,66 @@
 @extends('layouts.admin')
 
 @section('content')
-        <div class="backgroundHeader">
-            <header-section class="d-flex flex-column justify-content-center h-100">
-                <h1>Recensioni ricevute</h1>
-    
-                @if (count($reviews) == 0)
-                    <h2>Non ci sono recensioni da visualizzare</h2>
-                @endif
+    <div class="backgroundHeader">
+        <header-section class="d-flex flex-column justify-content-center h-100">
+            <h1>Recensioni ricevuti</h1>
+
+            @if (count($reviews) == 0)
+                <h2>Non ci sono recensioni da visualizzare</h2>
+            @endif
         </header-section>
-        </div>
-        
-        <section> 
-            <div class="row bg ps-5 pe-5">
-                @foreach ($reviews as $review)
-                    <div class="col-xs-12 col-md-6 col-xl-4 gap-2 p-2">
-                        <div class="card mb-3 pointer messandreview_bg " data-bs-toggle="modal"
-                            data-bs-target="#review{{ $loop->index }}">
-                            <div class="card-header h-50">
-                                <div class="card-title">
-                                    <h6><i class="fa-solid fa-comment"></i> </h6>
-                                    <p>{{ $review->name }}</p>
-                                </div>
-                            </div>
-                            <div class="card-body">
-                                {{ date('d-m-Y', strtotime($review->date)) }}
-                            </div>
-                        </div>
-                    </div>
-    
-    
-                    <div class="modal fade" id="review{{ $loop->index }}" data-bs-backdrop="static" data-bs-keyboard="false"
-                        tabindex="-1" aria-labelledby="Label{{ $loop->index }}" aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <div>
-                                        <h2 class="modal-title" id="reviewLabel{{ $loop->index }}"> {{ $review->name }}
-                                        </h2>
-                                        <h5>{{ $review->title }}</h5>
+    </div>
+
+    <!-- Pannello delle recensioni -->
+    <div class="card border-0">
+        <div class="card-body">
+            <table class="my-table table table-hover table-striped">
+                <thead>
+                    <tr>
+                        <th class="d-none d-md-table-cell">Da <i class="fa-solid fa-user"></th>
+                        <th>Titolo <i class="fa-solid fa-envelope"></i></th>
+                        <th class="d-none d-sm-table-cell">Data <i class="fa-regular fa-calendar"></i></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($reviews as $review)
+                        <tr class="messandreview_bg pointer " data-bs-toggle="modal"
+                            data-bs-target="#message{{ $loop->index }}">
+                            <td class="d-none d-md-table-cell">{{ $review->name }}</td>
+                            <td>{{ $review->title }}</td>
+                            <td class="d-none d-sm-table-cell">{{ $review->date }}</td>
+                        </tr>
+
+                        {{-- modale --}}
+                        <div class="modal fade bg" id="message{{ $loop->index }}" data-bs-backdrop="static"
+                            data-bs-keyboard="false" tabindex="-1" aria-labelledby="messageLabel{{ $loop->index }}"
+                            aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <div>
+                                            <h1 class="modal-title fs-5" id="messageLabel{{ $loop->index }}">
+                                                {{ $review->name }}
+                                            </h1>
+                                            <h5><a href="mailto:{{ $review->title }}">{{ $review->title }}</a>
+                                            </h5>
+                                        </div>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
                                     </div>
-                                    <button type="button" class="btn-close " data-bs-dismiss="modal"
-                                        aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <h5>Testo della recensione:</h5>
-                                    <p>{{ $review->comment }}</p>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn_close" data-bs-dismiss="modal">Close</button>
+                                    <div class="modal-body">
+                                        <h5>Testo del messaggio:</h5>
+                                        <p>{{ $review->comment }}</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn_close" data-bs-dismiss="modal">Close</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                @endforeach
-            </div>
-        </section>
-        
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
-
-
-
-
-
-
-
-
-
-
-
-{{-- @foreach ($reviews as $review)
-                <div class="col-12">
-                    <div class="card mb-3 ">
-                        <div class="card-header">
-                            <div class="card-title">
-                                <h4>{{ $review->title }}</h4>
-                            </div>
-                            <div class="card-subtitle">
-                                {{ $review->name }} <br>
-                                {{ $review->date }}
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            <p>
-                            <h5>Testo della recensione:</h5>
-                            {{ $review->comment }}
-                            </p>
-                        </div>
-
-                    </div>
-                </div>
-            @endforeach --}}
