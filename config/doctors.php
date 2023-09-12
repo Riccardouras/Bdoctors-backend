3a09bbc1ea19016af1219397c0e8209a0faa82b2
MatteoGaloni <mtgaloni@gmail.com>
2023-09-12 09:59:20 +0200

    add BartolomeoCv

--- config/doctors.php
+++ config/doctors.php
@@ -1,214 +1,214 @@
-<?php 
+<?php
 
 return [
-        [
-            'name'=> 'Andrea Grande',
-            'address'=>'Via del Magnanimo 7',
-            'city'=>'Roma',
-            'image'=>'uploads/medico.jpg',
-            'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3891681144',
-            'specialties' => ['1'],
-            'service' => 'Diagnosi e cura delle principali manifestazioni allergiche'
-        ],
-        [
-            'name'=> 'Bartolomeo Bartino',
-            'address'=>'Via Moro 81',
-            'city'=>'Treviso',
-            'image'=>'uploads/medico2.jpg',
-            'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3331118784',
-            'specialties' => ['2', '1'],
-            'service' => 'Esami istologici. Diagnosi e cura delle principali manifestazioni allergiche'
-        ],
-        [
-            'name'=> 'Carlo Magno',
-            'address'=>'Via Giotto 91',
-            'city'=>'Milano',
-            'image'=>'uploads/medico3.jpg',
-            'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3471094124',
-            'specialties' => ['3', '2', '1'],
-            'service' => 'Ripristino funzionalità epato-renale. Esami istologici. Diagnosi e cura delle principali manifestazioni allergiche'
-        ],
-        [
-            'name'=> 'Daniela Ligure',
-            'address'=>'Via Liguria 12',
-            'city'=>'Torino',
-            'image'=>'uploads/medico4.jpg',
-            'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'355127198',
-            'specialties' => ['4'],
-            'service' => 'Controllo al cuore.'
-        ],
-        [
-            'name'=> 'Eralda Bossi',
-            'address'=>'Via Sardegna 99',
-            'city'=>'Napoli',
-            'image'=>'uploads/medico5.jpg',
-            'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3201019222',
-            'specialties' => ['5'],
-            'service' => 'Controllo al cuore dei bambini'
-        ],
-        [
-            'name'=> 'Floriana Berlusconi',
-            'address'=>'Via del Santuario 9',
-            'city'=>'Campobasso',
-            'image'=>'uploads/medico16.jpg',
-            'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3819012455',
-            'specialties' => ['6'],
-            'service' => 'Intervento chirurgico cavità addominale'
-        ],
-        [
-            'name'=> 'Gabriele Rizzo',
-            'address'=>'Piazza Alcide De Gasperi 44',
-            'city'=>'Padova',
-            'image'=>'uploads/medico6.jpg',
-            'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3115648123',
-            'specialties' => ['2','7'],
-            'service' => 'Intervento chirurgico cavità addominale dei bambini'
-        ],
-        [
-            'name'=> 'Hermione Granger',
-            'address'=>'Via del Salmone',
-            'city'=>'Bari',
-            'image'=>'uploads/medico17.jpg',
-            'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3319089733',
-            'specialties' => ['8'],
-            'service' => 'Checkup dermatologico e dermatoscopie.'
-        ],
-        [
-            'name'=> 'Ilaria Condizionata',
-            'address'=>'Via del Mulino 81',
-            'city'=>'Mantova',
-            'image'=>'uploads/medico18.jpg',
-            'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3489088901',
-            'specialties' => ['9'],
-            'service' => 'Esami del sangue'
-        ],
-        [
-            'name'=> 'Laura Pausini',
-            'address'=>'Via del Sauro 12',
-            'city'=>'Sbrunchio sul mento',
-            'image'=>'uploads/medico19.jpg',
-            'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3411890123',
-            'specialties' => ['10'],
-            'service' => 'Visita di controllo'
-        ],
-        [
-            'name'=> 'Matteo Galoni',
-            'address'=>'Via Consueta 1',
-            'city'=>'Paese',
-            'image'=>'uploads/medico7.jpg',
-            'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3333333333',
-            'specialties' => ['2','11'],
-            'service' => 'Visita di controllo'
-        ],
-        [
-            'name'=> 'Norman Dia',
-            'address'=>'Via dell Hotel 18',
-            'city'=>'Firenze',
-            'image'=>'uploads/medico9.jpg',
-            'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3901990133',
-            'specialties' => ['12'],
-            'service' => 'Visita di controllo'
-        ],
-        [
-            'name'=> 'Orlanda Furiosa',
-            'address'=>'Via popolare',
-            'city'=>'Urbino',
-            'image'=>'uploads/medico8.jpg',
-            'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3489019202',
-            'specialties' => ['13'],
-            'service' => 'Visita di controllo'
-        ],
-        [
-            'name'=> 'Paolo Rossi',
-            'address'=>'Via del Fangario 23',
-            'city'=>'Cagliari',
-            'image'=>'uploads/medico10.jpg',
-            'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3891790189',
-            'specialties' => ['2','14'],
-            'service' => 'Visita di controllo'
-        ],
-        [
-            'name'=> 'Quasimodo Meloni',
-            'address'=>'Via del Poeta 45',
-            'city'=>'Genova',
-            'image'=>'uploads/medico11.jpg',
-            'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'37898091199',
-            'specialties' => ['15'],
-            'service' => 'Visita di controllo'
-        ],
-        [
-            'name'=> 'Rodolfo Valentino',
-            'address'=>'Via Ernesto Forza 17',
-            'city'=>'Palermo',
-            'image'=>'uploads/medico12.jpg',
-            'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3567809123',
-            'specialties' => ['16'],
-            'service' => 'Visita di controllo'
-        ],
-        [
-            'name'=> 'Sara Mai',
-            'address'=>'Via Giacomo Matteotti 26',
-            'city'=>'Potenza',
-            'image'=>'uploads/medico20.jpg',
-            'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3908918901',
-            'specialties' => ['17'],
-            'service' => 'Visita di controllo'
-        ],
-        [
-            'name'=> 'Tiziano Ferro',
-            'address'=>'Via dell Artista',
-            'city'=>'Siena',
-            'image'=>'uploads/medico13.jpg',
-            'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3789018909',
-            'specialties' => ['18'],
-            'service' => 'Visita di controllo'
-        ],
-        [
-            'name'=> 'Ubaldo Ubaldi',
-            'address'=>'Via delle Vele',
-            'city'=>'Perugia',
-            'image'=>'uploads/medico14.jpg',
-            'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3901890788',
-            'specialties' => ['19'],
-            'service' => 'Visita di controllo'
-        ],
-        [
-            'name'=> 'Valeria Caramellina',
-            'address'=>'Via della Montagna 12',
-            'city'=>'Udine',
-            'image'=>'uploads/medico21.jpg',
-            'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3389018902',
-            'specialties' => [],
-            'service' => 'Visita di controllo'
-        ],
-        [
-            'name'=> 'Zaira Orfei',
-            'address'=>'Via Marche 14',
-            'city'=>'Bolzano',
-            'image'=>'uploads/medico22.jpg',
-            'curriculum'=>'uploads/cvmedico.pdf',
-            'phone_number'=>'3891790188',
-            'specialties' => [],
-            'service' => 'Visita di controllo'
-        ]
+    [
+        'name' => 'Andrea Grande',
+        'address' => 'Via del Magnanimo 7',
+        'city' => 'Roma',
+        'image' => 'uploads/medico.jpg',
+        'curriculum' => 'uploads/cvmedico.pdf',
+        'phone_number' => '3891681144',
+        'specialties' => ['1'],
+        'service' => 'Diagnosi e cura delle principali manifestazioni allergiche'
+    ],
+    [
+        'name' => 'Bartolomeo Bartino',
+        'address' => 'Via Moro 81',
+        'city' => 'Treviso',
+        'image' => 'uploads/medico2.jpg',
+        'curriculum' => 'uploads/bartino_cv.pdf',
+        'phone_number' => '3331118784',
+        'specialties' => ['2', '1'],
+        'service' => 'Esami istologici. Diagnosi e cura delle principali manifestazioni allergiche'
+    ],
+    [
+        'name' => 'Carlo Magno',
+        'address' => 'Via Giotto 91',
+        'city' => 'Milano',
+        'image' => 'uploads/medico3.jpg',
+        'curriculum' => 'uploads/cvmedico.pdf',
+        'phone_number' => '3471094124',
+        'specialties' => ['3', '2', '1'],
+        'service' => 'Ripristino funzionalità epato-renale. Esami istologici. Diagnosi e cura delle principali manifestazioni allergiche'
+    ],
+    [
+        'name' => 'Daniela Ligure',
+        'address' => 'Via Liguria 12',
+        'city' => 'Torino',
+        'image' => 'uploads/medico4.jpg',
+        'curriculum' => 'uploads/cvmedico.pdf',
+        'phone_number' => '355127198',
+        'specialties' => ['4'],
+        'service' => 'Controllo al cuore.'
+    ],
+    [
+        'name' => 'Eralda Bossi',
+        'address' => 'Via Sardegna 99',
+        'city' => 'Napoli',
+        'image' => 'uploads/medico5.jpg',
+        'curriculum' => 'uploads/cvmedico.pdf',
+        'phone_number' => '3201019222',
+        'specialties' => ['5'],
+        'service' => 'Controllo al cuore dei bambini'
+    ],
+    [
+        'name' => 'Floriana Berlusconi',
+        'address' => 'Via del Santuario 9',
+        'city' => 'Campobasso',
+        'image' => 'uploads/medico16.jpg',
+        'curriculum' => 'uploads/cvmedico.pdf',
+        'phone_number' => '3819012455',
+        'specialties' => ['6'],
+        'service' => 'Intervento chirurgico cavità addominale'
+    ],
+    [
+        'name' => 'Gabriele Rizzo',
+        'address' => 'Piazza Alcide De Gasperi 44',
+        'city' => 'Padova',
+        'image' => 'uploads/medico6.jpg',
+        'curriculum' => 'uploads/cvmedico.pdf',
+        'phone_number' => '3115648123',
+        'specialties' => ['2', '7'],
+        'service' => 'Intervento chirurgico cavità addominale dei bambini'
+    ],
+    [
+        'name' => 'Hermione Granger',
+        'address' => 'Via del Salmone',
+        'city' => 'Bari',
+        'image' => 'uploads/medico17.jpg',
+        'curriculum' => 'uploads/cvmedico.pdf',
+        'phone_number' => '3319089733',
+        'specialties' => ['8'],
+        'service' => 'Checkup dermatologico e dermatoscopie.'
+    ],
+    [
+        'name' => 'Ilaria Condizionata',
+        'address' => 'Via del Mulino 81',
+        'city' => 'Mantova',
+        'image' => 'uploads/medico18.jpg',
+        'curriculum' => 'uploads/cvmedico.pdf',
+        'phone_number' => '3489088901',
+        'specialties' => ['9'],
+        'service' => 'Esami del sangue'
+    ],
+    [
+        'name' => 'Laura Pausini',
+        'address' => 'Via del Sauro 12',
+        'city' => 'Sbrunchio sul mento',
+        'image' => 'uploads/medico19.jpg',
+        'curriculum' => 'uploads/cvmedico.pdf',
+        'phone_number' => '3411890123',
+        'specialties' => ['10'],
+        'service' => 'Visita di controllo'
+    ],
+    [
+        'name' => 'Matteo Galoni',
+        'address' => 'Via Consueta 1',
+        'city' => 'Paese',
+        'image' => 'uploads/medico7.jpg',
+        'curriculum' => 'uploads/cvmedico.pdf',
+        'phone_number' => '3333333333',
+        'specialties' => ['2', '11'],
+        'service' => 'Visita di controllo'
+    ],
+    [
+        'name' => 'Norman Dia',
+        'address' => 'Via dell Hotel 18',
+        'city' => 'Firenze',
+        'image' => 'uploads/medico9.jpg',
+        'curriculum' => 'uploads/cvmedico.pdf',
+        'phone_number' => '3901990133',
+        'specialties' => ['12'],
+        'service' => 'Visita di controllo'
+    ],
+    [
+        'name' => 'Orlanda Furiosa',
+        'address' => 'Via popolare',
+        'city' => 'Urbino',
+        'image' => 'uploads/medico8.jpg',
+        'curriculum' => 'uploads/cvmedico.pdf',
+        'phone_number' => '3489019202',
+        'specialties' => ['13'],
+        'service' => 'Visita di controllo'
+    ],
+    [
+        'name' => 'Paolo Rossi',
+        'address' => 'Via del Fangario 23',
+        'city' => 'Cagliari',
+        'image' => 'uploads/medico10.jpg',
+        'curriculum' => 'uploads/cvmedico.pdf',
+        'phone_number' => '3891790189',
+        'specialties' => ['2', '14'],
+        'service' => 'Visita di controllo'
+    ],
+    [
+        'name' => 'Quasimodo Meloni',
+        'address' => 'Via del Poeta 45',
+        'city' => 'Genova',
+        'image' => 'uploads/medico11.jpg',
+        'curriculum' => 'uploads/cvmedico.pdf',
+        'phone_number' => '37898091199',
+        'specialties' => ['15'],
+        'service' => 'Visita di controllo'
+    ],
+    [
+        'name' => 'Rodolfo Valentino',
+        'address' => 'Via Ernesto Forza 17',
+        'city' => 'Palermo',
+        'image' => 'uploads/medico12.jpg',
+        'curriculum' => 'uploads/cvmedico.pdf',
+        'phone_number' => '3567809123',
+        'specialties' => ['16'],
+        'service' => 'Visita di controllo'
+    ],
+    [
+        'name' => 'Sara Mai',
+        'address' => 'Via Giacomo Matteotti 26',
+        'city' => 'Potenza',
+        'image' => 'uploads/medico20.jpg',
+        'curriculum' => 'uploads/cvmedico.pdf',
+        'phone_number' => '3908918901',
+        'specialties' => ['17'],
+        'service' => 'Visita di controllo'
+    ],
+    [
+        'name' => 'Tiziano Ferro',
+        'address' => 'Via dell Artista',
+        'city' => 'Siena',
+        'image' => 'uploads/medico13.jpg',
+        'curriculum' => 'uploads/cvmedico.pdf',
+        'phone_number' => '3789018909',
+        'specialties' => ['18'],
+        'service' => 'Visita di controllo'
+    ],
+    [
+        'name' => 'Ubaldo Ubaldi',
+        'address' => 'Via delle Vele',
+        'city' => 'Perugia',
+        'image' => 'uploads/medico14.jpg',
+        'curriculum' => 'uploads/cvmedico.pdf',
+        'phone_number' => '3901890788',
+        'specialties' => ['19'],
+        'service' => 'Visita di controllo'
+    ],
+    [
+        'name' => 'Valeria Caramellina',
+        'address' => 'Via della Montagna 12',
+        'city' => 'Udine',
+        'image' => 'uploads/medico21.jpg',
+        'curriculum' => 'uploads/cvmedico.pdf',
+        'phone_number' => '3389018902',
+        'specialties' => [],
+        'service' => 'Visita di controllo'
+    ],
+    [
+        'name' => 'Zaira Orfei',
+        'address' => 'Via Marche 14',
+        'city' => 'Bolzano',
+        'image' => 'uploads/medico22.jpg',
+        'curriculum' => 'uploads/cvmedico.pdf',
+        'phone_number' => '3891790188',
+        'specialties' => [],
+        'service' => 'Visita di controllo'
+    ]
 ];
